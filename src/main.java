e9d7a3da5289e1372485a25a3ab687886b18fb5b
GenuineJNguyen <johnson.nguyen59@yahoo.com>
2019-04-22 18:27:51 -0700

    Ignore the workspade.xml file

--- src/main.java
+++ src/main.java
@@ -1,5 +1,40 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Please login with your credentials to proceed\n" +
+                "Username:\ttempUser\n" +
+                "Password:\t*****\n" +
+                "-\tLogin SUCCESSFUL\t -\n" +
+                "\n" +
+                "Welcome to the Vending Machine Portal\n" +
+                "\n" +
+                "What would you like to do?\n" +
+                "View overall Vending Machine Stats\n" +
+                "View Statistics for a specific Vending Machine\n" +
+                "Log out and exit\n" +
+                "Please enter your choice: 1\n" +
+                "\t-\tDisplaying stats in a separate window... \t-\n" +
+                "\n" +
+                "What would you like to do?\n" +
+                "View overall Vending Machine Stats\n" +
+                "View Statistics for a specific Vending Machine\n" +
+                "Log out and exit\n" +
+                "Please enter your choice: 2\n" +
+                "\n" +
+                "Please enter the ID of the vending machine: 12345\n" +
+                "-\tESTABLISHING A CONNECTION...\t-\n" +
+                "\t-\tCONNECTED\t-\n" +
+                "What would you like to do with this vending machine\n" +
+                "Check Statistics\n" +
+                "Check Inventory\n" +
+                "Modify Vending Machine Items\n" +
+                "Go back to menu\n" +
+                "Please enter your choice: 4\n" +
+                "\n" +
+                "What would you like to do?\n" +
+                "View overall Vending Machine Stats\n" +
+                "View Statistics for a specific Vending Machine\n" +
+                "Log out and exit\n" +
+                "Please enter your choice: 3\n" +
+                "-\tLogging off\t -\n");
     }
 }
