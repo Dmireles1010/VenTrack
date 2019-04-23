9725f2411d1b7a0e4c0ad6192e45b85acd47a91a
DavidNguyen19 <49822535+DavidNguyen19@users.noreply.github.com>
2019-04-22 19:29:30 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -8,9 +8,6 @@ public class main {
         System.out.println("What would you like to do?\n");
         System.out.println("Brian");
         System.out.println("David Nguyen");
-        System.out.println("Johnson");
-        System.out.println("TESTING ");
-        System.out.println("David Mireles");
-        System.out.println("TESTING");
+
     }
 }
