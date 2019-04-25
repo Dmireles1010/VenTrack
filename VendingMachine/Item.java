419dc72fee6c2cf0b97fd950684424309b222640
DavidNguyen19 <49822535+DavidNguyen19@users.noreply.github.com>
2019-04-24 21:48:51 -0700

    vendinemachine

---
+++ VendingMachine/Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    String itemName;
+    double price;
+    int quantity;
+
+
+    public Item(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
