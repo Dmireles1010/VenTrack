cec80dc909c5ecb6377a857d4200d15573681051
B-RyeTheAsianSpy <brian.edison.nguyen@gmail.com>
2019-04-27 08:54:35 -0700

    Revert "Merge branch 'master' of https://github.com/GenuineJNguyen/VenTrack"
    This reverts commit 62dd4493c0308d3de3c13f369730d19d1a145641, reversing
    changes made to 537c3054edfb141bedb036e94000b41cb01d2814.

--- src/Item.java
+++ src/Item.java
@@ -1,12 +1,2 @@
 public class Item {
-    String itemName;
-    double price;
-    int quantity;
-
-
-    public Item(String itemName, double price, int quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
